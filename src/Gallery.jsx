66239d2b13889e10ba74dedd403cf79933ade7fc
akolle <andykoller55@yahoo.com>
2024-08-23 21:36:03 -0400

    query images

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,4 +1,20 @@
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
+
+const url =
+  'https://api.unsplash.com/search/photos?client_id=B4ZLSOzMaomVLddUzD8TD1rqyzjqGF8fChzXJCi0w6o&query=office'
+
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const result = await axios.get(url)
+
+      return result.data
+    },
+  })
+
+  console.log(response)
   return <h2>Gallery</h2>
 }
 export default Gallery

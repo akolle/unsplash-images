e49f31e7bb1b715e052f456a9994efca574803f5
akolle <andykoller55@yahoo.com>
2024-08-23 21:44:32 -0400

    rending photos

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useQuery } from '@tanstack/react-query'
 
 const url =
-  'https://api.unsplash.com/search/photos?client_id=B4ZLSOzMaomVLddUzD8TD1rqyzjqGF8fChzXJCi0w6o&query=office'
+  'https://api.unsplash.com/search/photos?client_id=B4ZLSOzMaomVLddUzD8TD1rqyzjqGF8fChzXJCi0w6o&query=redpanda'
 
 const Gallery = () => {
   const response = useQuery({
@@ -14,7 +14,45 @@ const Gallery = () => {
     },
   })
 
-  console.log(response)
-  return <h2>Gallery</h2>
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    )
+  }
+
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <h4>There was an error...</h4>
+      </section>
+    )
+  }
+
+  const results = response.data.results
+
+  if (results.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>No results found...</h4>
+      </section>
+    )
+  }
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular
+        return (
+          <img
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+            className="img"
+          />
+        )
+      })}
+    </section>
+  )
 }
 export default Gallery

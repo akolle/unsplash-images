be1f8cf8d44a811508374757634add57a7929a01
akolle <andykoller55@yahoo.com>
2024-08-23 21:28:37 -0400

    setting up react query

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { AppProvider } from './context'
 import App from './App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AppProvider>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </AppProvider>
 )

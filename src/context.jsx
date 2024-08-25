b1297fc0b55446f0df5e103459a82a59607c6322
akolle <andykoller55@yahoo.com>
2024-08-25 19:29:49 -0400

    fix for dark theme on phone

--- src/context.jsx
+++ src/context.jsx
@@ -2,21 +2,22 @@ import { createContext, useContext, useState, useEffect } from 'react'
 
 const AppContext = createContext()
 
-const getInitialDarkMode = () => {
-  const prefersDarkMode = window.matchMedia(
-    '(prefers-color-scheme:dark)'
-  ).matches
+// const getInitialDarkMode = () => {
+//   const prefersDarkMode = window.matchMedia(
+//     '(prefers-color-scheme:dark)'
+//   ).matches
 
-  const storedDarkMode = localStorage.getItem('darkTheme')
+//   const storedDarkMode = localStorage.getItem('darkTheme')
 
-  if (storedDarkMode === null) {
-    return prefersDarkMode
-  }
-  return storedDarkMode === 'true'
-}
+//   if (storedDarkMode === null) {
+//     return prefersDarkMode
+//   }
+//   return storedDarkMode === 'true'
+// }
 
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode())
+  // const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode())
+  const [isDarkTheme, setIsDarkTheme] = useState(false)
   const [searchTerm, setSearchTerm] = useState('cat')
 
   const toggleDarkTheme = () => {

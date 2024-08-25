85e7429a65cbfc70bde4e1b89b3f4e4472ad3b19
akolle <andykoller55@yahoo.com>
2024-08-24 20:48:44 -0400

    local storage

--- src/context.jsx
+++ src/context.jsx
@@ -6,18 +6,23 @@ const getInitialDarkMode = () => {
   const prefersDarkMode = window.matchMedia(
     '(prefers-color-scheme:dark)'
   ).matches
-  console.log(prefersDarkMode)
-  return prefersDarkMode
+
+  const storedDarkMode = localStorage.getItem('darkTheme')
+
+  if (storedDarkMode === null) {
+    return prefersDarkMode
+  }
+  return storedDarkMode === 'true'
 }
 
 export const AppProvider = ({ children }) => {
   const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode())
-
   const [searchTerm, setSearchTerm] = useState('cat')
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme
     setIsDarkTheme(newDarkTheme)
+    localStorage.setItem('darkTheme', newDarkTheme)
   }
 
   useEffect(() => {
